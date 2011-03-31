b5b382fd780592b1446c7d1c7b496d669d9eb9f1
Kim Hunter <bigkm1@gmail.com>
2011-03-31 22:23:20 +1000

    Added tests for is left on or right function

--- test.rb
+++ test.rb
@@ -31,7 +31,19 @@ class TestEdge < Test::Unit::TestCase
     @d_edge = Edge.new(Point.new(0,0), Point.new(10,10))
   end
  
-
+  def test_vertical_edge_is_point_left_on_or_right
+    assert_equal 0, @v_edge.is_point_left_on_or_right(Point.new(0,0)), "point should be on vertical line"
+    assert 0 < @v_edge.is_point_left_on_or_right(Point.new(12,0)), "point should be on right "
+    assert 0 > @v_edge.is_point_left_on_or_right(Point.new(-0.2,0)), "point should be on left"
+  end
+  
+  def test_diagonal_edge_is_point_left_on_or_right
+    # Diagonal
+    assert 0 > @d_edge.is_point_left_on_or_right(Point.new(0,1))
+    assert 0 < @d_edge.is_point_left_on_or_right(Point.new(0,-1))
+    assert 0 < @d_edge.is_point_left_on_or_right(Point.new(10,0))    
+  end
+  
   def test_start_end_points
     p1 = Point.new(0,0)
     p2 = Point.new(0,20)
@@ -39,7 +51,8 @@ class TestEdge < Test::Unit::TestCase
     assert_equal(p1, edge.start_point)
     assert_equal(p2, edge.end_point)
   end
-
+  
+  
 
   def test_rise
     assert_equal 10, @v_edge.rise ,"Rise is wrong for vertical line"
